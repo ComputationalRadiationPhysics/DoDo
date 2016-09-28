98c22b551c1b27de73cd51e898168e57370d8bd8
Carlchristian Eckert <Carli-Eckert@gmx.de>
2016-09-28 17:57:52 +0200

    modified GOL benchmark for coding style

--- examples/benchmark_gol_code_overhead/src/main.cpp
+++ examples/benchmark_gol_code_overhead/src/main.cpp
@@ -3,16 +3,24 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
+#include <dodo2.hpp>
 
-void print(int,int,MPI_Comm &);
-void updateAlive(int&, std::array<int,4>&, MPI_Comm &);
+
+void print(
+    int,
+    int,
+    MPI_Comm &
+);
+void updateAlive(
+    int &,
+    std::array< int, 4 > &,
+    MPI_Comm &
+);
 void findNeighbors(
     std::array< int, 2 > const &,
     std::array< int, 4 > &,
     MPI_Comm &
 );
-
-
 void
 print(
     int rank,
@@ -22,16 +30,31 @@ print(
 {
     if( rank != 0 )
     {
-        MPI_Send( &alive, 1, MPI_INT, 0, 1, comm );
+        MPI_Send(
+            &alive,
+            1,
+            MPI_INT,
+            0,
+            1,
+            comm
+        );
     }
     else
     {
         std::array< int, 16 > alive_states;
         alive_states[0] = alive;
         MPI_Status status;
-        for( size_t i = 1; i < 16; ++i )
+        for( size_t i = 1 ; i < 16 ; ++i )
         {
-            MPI_Recv( &alive_states[i], 1, MPI_INT, static_cast<int>(i), 1, comm, &status );
+            MPI_Recv(
+                &alive_states[i],
+                1,
+                MPI_INT,
+                static_cast< int >( i ),
+                1,
+                comm,
+                &status
+            );
         }
         for( size_t i = 0; i < 16; ++i )
         {
@@ -44,16 +67,24 @@ print(
         std::cout << std::endl;
     }
 }
+
 void
 updateAlive(
     int & alive,
-    std::array<int,4> & neighborRanks,
+    std::array< int, 4 > & neighborRanks,
     MPI_Comm & comm
 )
 {
     for( auto r : neighborRanks )
     {
-        MPI_Send( &alive, 1, MPI_INT, r, 0, comm );
+        MPI_Send(
+            &alive,
+            1,
+            MPI_INT,
+            r,
+            0,
+            comm
+        );
     }
     int aliveN = 0;
     for( auto r : neighborRanks )
@@ -61,8 +92,19 @@ updateAlive(
         (void)r;
         int rX=0;
         MPI_Status status;
-        MPI_Recv( &rX, 1, MPI_INT, r, 0, comm, &status );
-        if(status.MPI_ERROR != 0) throw std::runtime_error("error");
+        MPI_Recv(
+            &rX,
+            1,
+            MPI_INT,
+            r,
+            0,
+            comm,
+            &status
+        );
+        if( status.MPI_ERROR != 0 )
+        {
+            throw std::runtime_error( "error" );
+        }
         aliveN += rX;
     }
     if( aliveN == 2 )
@@ -87,32 +129,131 @@ findNeighbors(
     int id;
     std::array< int, 2 > neighborCoord; //North
     neighborCoord = { { coord[0], ( coord[1] + 3 ) % 4 } };
-    MPI_Cart_rank( comm, neighborCoord.data( ), &id );
+    MPI_Cart_rank(
+        comm,
+        neighborCoord.data(),
+        &id
+    );
     neighborRanks[0] = id;
 
     //East
     neighborCoord = { { ( coord[0] + 1 ) % 4, coord[1] } };
-    MPI_Cart_rank( comm, neighborCoord.data( ), &id );
+    MPI_Cart_rank(
+        comm,
+        neighborCoord.data(),
+        &id
+    );
     neighborRanks[1] = id;
 
     //South
     neighborCoord = { { coord[0], ( coord[1] + 1 ) % 4 } };
-    MPI_Cart_rank( comm, neighborCoord.data( ), &id );
+    MPI_Cart_rank(
+        comm,
+        neighborCoord.data(),
+        &id
+    );
     neighborRanks[2] = id;
 
     //West
     neighborCoord = { { ( coord[0] + 3 ) % 4, coord[1] } };
-    MPI_Cart_rank( comm, neighborCoord.data( ), &id );
+    MPI_Cart_rank(
+        comm,
+        neighborCoord.data(),
+        &id
+    );
     neighborRanks[3] = id;
 }
 
 
-/* A two-dimensional torus of 16 processes on a square grid */
+// A two-dimensional torus of 16 processes on a square grid
 int main(
     int argc,
     char *argv[]
 )
 {
+    //typedef//xx
+    using MyHWAbs = dodo::model::hardware::HardwareAbstraction< //xx
+        dodo::model::hardware::extension::VertexSpeed,//xx
+        dodo::model::hardware::extension::InterconnectBandwidth//xx
+    >;//xx
+    auto hwa = std::make_shared<MyHWAbs>();//xx
+
+    // Hardware architecture//xx
+    auto rootNode = hwa->addRoot(//xx
+        "Hypnos",//xx
+        dodo::model::hardware::property::VertexType::STRUCTURAL//xx
+    );//xx
+    std::vector<MyHWAbs::HardwareID> computeNodes(n);//xx
+    {//xx
+        const boost::array< std::size_t, 2 > lengths = { { 2,2 } };//xx
+        const boost::grid_graph< 2 > grid(//xx
+            lengths,//xx
+            { { true, true} }//xx
+        );//xx
+        for( size_t i=0; i<n ; ++i)//xx
+        {//xx
+            // each compute node gets 4 cores//xx
+            computeNodes[i] = hwa->add(//xx
+                "CompNode",//xx
+                dodo::model::hardware::property::VertexType::MACHINE,//xx
+                rootNode//xx
+            );//xx
+            for( int j = 0 ; j < 4 ; ++j )//xx
+            {//xx
+                auto core = hwa->add(//xx
+                    "Core",//xx
+                    dodo::model::hardware::property::VertexType::COMPUTE,//xx
+                    computeNodes[i]//xx
+                );//xx
+                hwa->setProperty(//xx
+                    "VertexSpeed",//xx
+                    core,//xx
+                    2400//xx
+                );//xx
+                auto edge = hwa->addInterconnectBidirectional(//xx
+                    computeNodes[i],//xx
+                    core,//xx
+                    "PCI"//xx
+                );//xx
+                hwa->setProperty(//xx
+                    "InterconnectBandwidth",//xx
+                    edge,//xx
+                    100000//xx
+                );//xx
+            }//xx
+        }//xx
+        for( decltype( n ) vid = 0 ; vid < n ; ++vid )//xx
+        {//xx
+            // compute nodes are connected through a 2D torus network//xx
+            const auto treeID = computeNodes[vid];//xx
+            const auto gridVertex = vertex(//xx
+                vid,//xx
+                grid//xx
+            );//xx
+            for( auto & e : boost::make_iterator_range( out_edges(//xx
+                            gridVertex,//xx
+                            grid//xx
+                        ) ) )//xx
+            {//xx
+                const auto toVertexInGrid = get(//xx
+                    boost::vertex_index,//xx
+                    grid,//xx
+                    e.second//xx
+                );//xx
+                auto const toTreeID = computeNodes[ toVertexInGrid ];//xx
+                auto const edge = hwa->addInterconnect(//xx
+                    treeID,//xx
+                    toTreeID,//xx
+                    "Infiniband"//xx
+                );//xx
+                hwa->setProperty(//xx
+                    "InterconnectBandwidth",//xx
+                    edge,//xx
+                    1000//xx
+                );//xx
+            }//xx
+        }//xx
+    }//xx
     int rank = -1;
     int nRanks = 0;
     std::array< int, 2 > dim( { { 4, 4 } } );
@@ -123,39 +264,116 @@ int main(
 
     // initialization
     MPI_Comm comm;
-    MPI_Init( &argc, &argv );
-    MPI_Comm_rank( MPI_COMM_WORLD, &rank );
-    MPI_Comm_size( MPI_COMM_WORLD, &nRanks );
+    MPI_Init(
+        &argc,
+        &argv
+    );
+    MPI_Comm_rank(
+        MPI_COMM_WORLD,
+        &rank
+    );
+    MPI_Comm_size(
+        MPI_COMM_WORLD,
+        &nRanks
+    );
     if( nRanks != 16 )
     {
         std::cerr << "Use 16 MPI Processes!" << std::endl;
-        MPI_Abort( MPI_COMM_WORLD, 1 );
+        MPI_Abort(
+            MPI_COMM_WORLD,
+            1
+        );
     }
 
-    MPI_Cart_create( MPI_COMM_WORLD, 2, dim.data( ), boundaryCondition.data( ), 1, &comm );
-    MPI_Cart_coords( comm, rank, 2, coord.data( ) );
+
+    MPI_Cart_create(
+        MPI_COMM_WORLD,
+        2,
+        dim.data( ),
+        boundaryCondition.data( ),
+        1,
+        &comm
+    );
+
+    // Model the MPI ranks as workers.//xx
+    // 4 Workers on each node (one per Core)//xx
+    auto workerModel = std::make_shared< dodo::model::worker::Model >( );//xx
+    dodo::mapping::worker2hardware::Interface< MyHWAbs > worker2hwMapping(//xx
+        workerModel,//xx
+        hwa//xx
+    );//xx
+    worker2hwMapping.generateTrivialMapping(//xx
+        1,//xx
+        { "CompNode" }//xx
+    );//xx
+    assert(//xx
+        dodo::mapping::worker2hardware::checkMemoryLegality( worker2hwMapping )//xx
+    );//xx
+
+    MPI_Cart_coords(
+        comm,
+        rank,
+        2,
+        coord.data( )
+    );
+    findNeighbors(
+        coord,
+        neighborRanks,
+        comm
+    );
+
     std::srand( static_cast< unsigned >( rank ) );
     alive = std::rand( ) % 2;
-    findNeighbors( coord, neighborRanks, comm );
 
-//    std::cout << rank;
-//    for(auto r : neighborRanks)
-//    {
-//        std::cout << " " << r;
-//    }
-//    std::cout << std::endl;
-
+    auto physDom = dodo::model::data::WrappedGrid2D::generate(//xx
+        4,//xx
+        4//xx
+    );//xx
+    auto dataAbstraction = std::make_shared<//xx
+        dodo::model::data::Abstraction<dodo::model::data::WrappedGrid3D>//xx
+    >( physDom );//xx
+    dodo::model::data::DataDomain livelinessStates;//xx
+    for(auto & v : boost::make_iterator_range( physDom.g.getVertices( ) ) )//xx
+    {//xx
+        auto d = livelinessStates.createDataAtPos(v);//xx
+        livelinessStates.setProperty(//xx
+            "sizeInKB",//xx
+            d,//xx
+            0.004f//xx
+        );//xx
+    }//xx
+    dataAbstraction->addDataDomain(//xx
+        std::move( livelinessStates ),//xx
+        "livelinessStates"//xx
+    );//xx
+    auto stateMap = dodo::mapping::data2worker::generateRoundRobin(//xx
+        livelinessStates,//xx
+        *workerModel//xx
+    );//xx
+    std::map< std::string, decltype(stateMap) > finalMap;//xx
+    finalMap["livelinessStates"] = stateMap;//xx
+    dodo::mapping::data2worker::Interface<//xx
+        dodo::model::data::WrappedGrid2D//xx
+    > data2workerMapping(//xx
+        dataAbstraction,//xx
+        workerModel,//xx
+        finalMap//xx
+    );//xx
 
     // Start simulation
-
-    for( int timestep = 0; timestep < 10; ++timestep )
+    for( int timestep = 0 ; timestep < 10 ; ++timestep )
     {
-        print( rank, alive, comm );
-        updateAlive( alive, neighborRanks, comm );
+        print(
+            rank,
+            alive,
+            comm
+        );
+        updateAlive(
+            alive,
+            neighborRanks,
+            comm
+        );
     }
-
     MPI_Finalize( );
     return 0;
 }
-
-
