7fadf368c66f687b38f604ed3db1d76a7307bda2
Carlchristian Eckert <Carli-Eckert@gmx.de>
2016-10-18 13:00:47 +0200

    changed GOL example for more specific markup comments

--- examples/benchmark_gol_code_overhead/src/main.cpp
+++ examples/benchmark_gol_code_overhead/src/main.cpp
@@ -6,37 +6,37 @@
 #include <dodo2.hpp>
 
 
-template< typename T_DataModel >//xx
-struct GOLRule ://xx
-    dodo::model::routine::ComponentBase< T_DataModel >//xx
-{//xx
-    virtual//xx
-    float//xx
-    effort(//xx
-        std::shared_ptr< T_DataModel > dataModel//xx
-    ) override//xx
-    {//xx
-        (void) dataModel;//xx
-        return 10;//xx
-    }//xx
-};//xx
+template< typename T_DataModel >//x4x
+struct GOLRule ://x4x
+    dodo::model::routine::ComponentBase< T_DataModel >//x4x
+{//x4x
+    virtual//x4x
+    float//x4x
+    effort(//x4x
+        std::shared_ptr< T_DataModel > dataModel//x4x
+    ) override//x4x
+    {//x4x
+        (void) dataModel;//x4x
+        return 10;//x4x
+    }//x4x
+};//x4x
 
-template< typename T_DataModel >//xx
-struct GOLPrint ://xx
-    dodo::model::routine::ComponentBase< T_DataModel >//xx
-{//xx
-    virtual//xx
-    float//xx
-    effort(//xx
-        std::shared_ptr< T_DataModel > dataModel//xx
-    ) override//xx
-    {//xx
-        return dataModel->template getProperty< float >(//xx
-            this->inData[0],//xx
-            "sizeInKB"//xx
-        ) * 8;//xx
-    }//xx
-};//xx
+template< typename T_DataModel >//x4x
+struct GOLPrint ://x4x
+    dodo::model::routine::ComponentBase< T_DataModel >//x4x
+{//x4x
+    virtual//x4x
+    float//x4x
+    effort(//x4x
+        std::shared_ptr< T_DataModel > dataModel//x4x
+    ) override//x4x
+    {//x4x
+        return dataModel->template getProperty< float >(//x4x
+            this->inData[0],//x4x
+            "sizeInKB"//x4x
+        ) * 8;//x4x
+    }//x4x
+};//x4x
 
 void print(
     int,
@@ -203,108 +203,108 @@ int main(
     char *argv[]
 )
 {
-    //typedef//xx
-    using MyHWAbs = dodo::model::hardware::HardwareAbstraction< //xx
-        dodo::model::hardware::extension::VertexSpeed,//xx
-        dodo::model::hardware::extension::InterconnectBandwidth//xx
-    >;//xx
-    auto hwa = std::make_shared< MyHWAbs >( );//xx
+    //typedef//x1x
+    using MyHWAbs = dodo::model::hardware::HardwareAbstraction< //x1x
+        dodo::model::hardware::extension::VertexSpeed,//x1x
+        dodo::model::hardware::extension::InterconnectBandwidth//x1x
+    >;//x1x
+    auto hwa = std::make_shared< MyHWAbs >( );//x1x
 
-    // Hardware architecture//xx
-    auto rootNode = hwa->addRoot(//xx
-        "Hypnos",//xx
-        dodo::model::hardware::property::VertexType::STRUCTURAL//xx
-    );//xx
-    std::vector< MyHWAbs::HardwareID > computeNodes( 4 );//xx
-    {//xx
-        const boost::array<//xx
-            std::size_t,//xx
-            2//xx
-        > lengths = { { 2, 2 } };//xx
-        const boost::grid_graph< 2 > grid(//xx
-            lengths,//xx
-            { { true, true } }//xx
-        );//xx
-        for( size_t i = 0; i < computeNodes.size( ); ++i )//xx
-        {//xx
-            // each compute node gets 4 cores//xx
-            computeNodes[i] = hwa->add(//xx
-                "CompNode",//xx
-                dodo::model::hardware::property::VertexType::MACHINE,//xx
-                rootNode//xx
-            );//xx
-            auto memory = hwa->add(//xx
-                "RAM",//xx
-                dodo::model::hardware::property::VertexType::MEMORY,//xx
-                computeNodes[i]//xx
-            );//xx
-            auto edge = hwa->addInterconnectBidirectional(//xx
-                memory,//xx
-                computeNodes[i],//xx
-                "PCIe"//xx
-            );//xx
-            for( int j = 0; j < 4; ++j )//xx
-            {//xx
-                auto core = hwa->add(//xx
-                    "Core",//xx
-                    dodo::model::hardware::property::VertexType::COMPUTE,//xx
-                    computeNodes[i]//xx
-                );//xx
-                hwa->setProperty(//xx
-                    "VertexSpeed",//xx
-                    core,//xx
-                    std::size_t( 2400 )//xx
-                );//xx
-                auto mEdge = hwa->addInterconnectBidirectional(//xx
-                    memory,//xx
-                    core,//xx
-                    "FSB"//xx
-                );//xx
-                hwa->setProperty(//xx
-                    "InterconnectBandwidth",//xx
-                    mEdge,//xx
-                    std::size_t( 100000 )//xx
-                );//xx
-                hwa->addToMemHierarchy(//xx
-                    core,//xx
-                    memory//xx
-                );//xx
-            }//xx
-        }//xx
-        for( unsigned vid = 0; vid < computeNodes.size( ); ++vid )//xx
-        {//xx
-            // compute nodes are connected through a 2D torus network//xx
-            const auto treeID = computeNodes[vid];//xx
-            const auto gridVertex = vertex(//xx
-                vid,//xx
-                grid//xx
-            );//xx
-            for( auto e : boost::make_iterator_range(//xx
-                out_edges(//xx
-                    gridVertex,//xx
-                    grid//xx
-                )//xx
-            ) )//xx
-            {//xx
-                const auto toVertexInGrid = get(//xx
-                    boost::vertex_index,//xx
-                    grid,//xx
-                    e.second//xx
-                );//xx
-                auto const toTreeID = computeNodes[toVertexInGrid];//xx
-                auto edge = hwa->addInterconnect(//xx
-                    treeID,//xx
-                    toTreeID,//xx
-                    "Infiniband"//xx
-                );//xx
-                hwa->setProperty(//xx
-                    std::string( "InterconnectBandwidth" ),//xx
-                    edge,//xx
-                    std::size_t( 1000 )//xx
-                );//xx
-            }//xx
-        }//xx
-    }//xx
+    // Hardware architecture//x1x
+    auto rootNode = hwa->addRoot(//x1x
+        "Hypnos",//x1x
+        dodo::model::hardware::property::VertexType::STRUCTURAL//x1x
+    );//x1x
+    std::vector< MyHWAbs::HardwareID > computeNodes( 4 );//x1x
+    {//x1x
+        const boost::array<//x1x
+            std::size_t,//x1x
+            2//x1x
+        > lengths = { { 2, 2 } };//x1x
+        const boost::grid_graph< 2 > grid(//x1x
+            lengths,//x1x
+            { { true, true } }//x1x
+        );//x1x
+        for( size_t i = 0; i < computeNodes.size( ); ++i )//x1x
+        {//x1x
+            // each compute node gets 4 cores//x1x
+            computeNodes[i] = hwa->add(//x1x
+                "CompNode",//x1x
+                dodo::model::hardware::property::VertexType::MACHINE,//x1x
+                rootNode//x1x
+            );//x1x
+            auto memory = hwa->add(//x1x
+                "RAM",//x1x
+                dodo::model::hardware::property::VertexType::MEMORY,//x1x
+                computeNodes[i]//x1x
+            );//x1x
+            auto edge = hwa->addInterconnectBidirectional(//x1x
+                memory,//x1x
+                computeNodes[i],//x1x
+                "PCIe"//x1x
+            );//x1x
+            for( int j = 0; j < 4; ++j )//x1x
+            {//x1x
+                auto core = hwa->add(//x1x
+                    "Core",//x1x
+                    dodo::model::hardware::property::VertexType::COMPUTE,//x1x
+                    computeNodes[i]//x1x
+                );//x1x
+                hwa->setProperty(//x1x
+                    "VertexSpeed",//x1x
+                    core,//x1x
+                    std::size_t( 2400 )//x1x
+                );//x1x
+                auto mEdge = hwa->addInterconnectBidirectional(//x1x
+                    memory,//x1x
+                    core,//x1x
+                    "FSB"//x1x
+                );//x1x
+                hwa->setProperty(//x1x
+                    "InterconnectBandwidth",//x1x
+                    mEdge,//x1x
+                    std::size_t( 100000 )//x1x
+                );//x1x
+                hwa->addToMemHierarchy(//x1x
+                    core,//x1x
+                    memory//x1x
+                );//x1x
+            }//x1x
+        }//x1x
+        for( unsigned vid = 0; vid < computeNodes.size( ); ++vid )//x1x
+        {//x1x
+            // compute nodes are connected through a 2D torus network//x1x
+            const auto treeID = computeNodes[vid];//x1x
+            const auto gridVertex = vertex(//x1x
+                vid,//x1x
+                grid//x1x
+            );//x1x
+            for( auto e : boost::make_iterator_range(//x1x
+                out_edges(//x1x
+                    gridVertex,//x1x
+                    grid//x1x
+                )//x1x
+            ) )//x1x
+            {//x1x
+                const auto toVertexInGrid = get(//x1x
+                    boost::vertex_index,//x1x
+                    grid,//x1x
+                    e.second//x1x
+                );//x1x
+                auto const toTreeID = computeNodes[toVertexInGrid];//x1x
+                auto edge = hwa->addInterconnect(//x1x
+                    treeID,//x1x
+                    toTreeID,//x1x
+                    "Infiniband"//x1x
+                );//x1x
+                hwa->setProperty(//x1x
+                    std::string( "InterconnectBandwidth" ),//x1x
+                    edge,//x1x
+                    std::size_t( 1000 )//x1x
+                );//x1x
+            }//x1x
+        }//x1x
+    }//x1x
     int rank = -1;
     int nRanks = 0;
     std::array< int, 2 > dim ( { { 4, 4 } } );
@@ -346,20 +346,20 @@ int main(
         &comm
     );
 
-    // Model the MPI ranks as workers.//xx
-    // 4 Workers on each node (one per Core)//xx
-    auto workerModel = std::make_shared< dodo::model::worker::Model >( );//xx
-    dodo::mapping::worker2hardware::Interface< MyHWAbs > worker2hwMapping(//xx
-        workerModel,//xx
-        hwa//xx
-    );//xx
-    worker2hwMapping.generateTrivialMapping(//xx
-        1,//xx
-        { "CompNode" }//xx
-    );//xx
-    assert(//xx
-        dodo::mapping::worker2hardware::checkMemoryLegality( worker2hwMapping )//xx
-    );//xx
+    // Model the MPI ranks as workers.//x2x
+    // 4 Workers on each node (one per Core)//x2x
+    auto workerModel = std::make_shared< dodo::model::worker::Model >( );//x2x
+    dodo::mapping::worker2hardware::Interface< MyHWAbs > worker2hwMapping(//x2x
+        workerModel,//x2x
+        hwa//x2x
+    );//x2x
+    worker2hwMapping.generateTrivialMapping(//x2x
+        1,//x2x
+        { "CompNode" }//x2x
+    );//x2x
+    assert(//x2x
+        dodo::mapping::worker2hardware::checkMemoryLegality( worker2hwMapping )//x2x
+    );//x2x
 
     MPI_Cart_coords(
         comm,
@@ -376,103 +376,103 @@ int main(
     std::srand( static_cast< unsigned >( rank ) );
     alive = std::rand( ) % 2;
 
-    using PhysicalDomain = dodo::model::data::WrappedGrid2D;//xx
-    auto physDom = PhysicalDomain::generate(//xx
-        4,//xx
-        4//xx
-    );//xx
-    using DataAbstraction = dodo::model::data::Abstraction< PhysicalDomain >;//xx
-    auto dataAbstraction = std::make_shared< DataAbstraction >( physDom );//xx
-    dodo::model::data::DataDomain livelinessStates;//xx
-    for( auto v : boost::make_iterator_range( physDom.g.getVertices( ) ) )//xx
-    {//xx
-        auto d = livelinessStates.createDataAtPos( v );//xx
-        livelinessStates.setProperty(//xx
-            "sizeInKB",//xx
-            d,//xx
-            0.004f//xx
-        );//xx
-    }//xx
-    dataAbstraction->addDataDomain(//xx
-        std::move( livelinessStates ),//xx
-        "livelinessStates"//xx
-    );//xx
-    auto stateMap = dodo::mapping::data2worker::generateRoundRobin(//xx
-        livelinessStates,//xx
-        *workerModel//xx
-    );//xx
-    std::map<//xx
-        std::string,//xx
-        decltype( stateMap )//xx
-    > finalMap;//xx
-    finalMap["livelinessStates"] = stateMap;//xx
-    dodo::mapping::data2worker::Interface< PhysicalDomain >//xx
-        data2workerMapping(//xx
-        dataAbstraction,//xx
-        workerModel,//xx
-        finalMap//xx
-    );//xx
+    using PhysicalDomain = dodo::model::data::WrappedGrid2D;//x3x
+    auto physDom = PhysicalDomain::generate(//x3x
+        4,//x3x
+        4//x3x
+    );//x3x
+    using DataAbstraction = dodo::model::data::Abstraction< PhysicalDomain >;//x3x
+    auto dataAbstraction = std::make_shared< DataAbstraction >( physDom );//x3x
+    dodo::model::data::DataDomain livelinessStates;//x3x
+    for( auto v : boost::make_iterator_range( physDom.g.getVertices( ) ) )//x3x
+    {//x3x
+        auto d = livelinessStates.createDataAtPos( v );//x3x
+        livelinessStates.setProperty(//x3x
+            "sizeInKB",//x3x
+            d,//x3x
+            0.004f//x3x
+        );//x3x
+    }//x3x
+    dataAbstraction->addDataDomain(//x3x
+        std::move( livelinessStates ),//x3x
+        "livelinessStates"//x3x
+    );//x3x
+    auto stateMap = dodo::mapping::data2worker::generateRoundRobin(//x3x
+        livelinessStates,//x3x
+        *workerModel//x3x
+    );//x3x
+    std::map<//x3x
+        std::string,//x3x
+        decltype( stateMap )//x3x
+    > finalMap;//x3x
+    finalMap["livelinessStates"] = stateMap;//x3x
+    dodo::mapping::data2worker::Interface< PhysicalDomain >//x3x
+        data2workerMapping(//x3x
+        dataAbstraction,//x3x
+        workerModel,//x3x
+        finalMap//x3x
+    );//x3x
 
-    using Directions = dodo::model::data::traits::Directions< dodo::model::data::WrappedGrid2D >::Values;//xx
-    using PortType = dodo::model::routine::Port< Directions >;//xx
-    dodo::model::routine::ComponentTemplate<//xx
-        GOLRule< DataAbstraction >//xx
-    > golRuleStencil;//xx
-    golRuleStencil.name = "livelinessRule";//xx
-    golRuleStencil.outPorts//xx
-        .push_back(//xx
-            PortType(//xx
-                "livelinessStates",//xx
-                Directions::SELF//xx
-            )//xx
-        );//xx
+    using Directions = dodo::model::data::traits::Directions< dodo::model::data::WrappedGrid2D >::Values;//x4x
+    using PortType = dodo::model::routine::Port< Directions >;//x4x
+    dodo::model::routine::ComponentTemplate<//x4x
+        GOLRule< DataAbstraction >//x4x
+    > golRuleStencil;//x4x
+    golRuleStencil.name = "livelinessRule";//x4x
+    golRuleStencil.outPorts//x4x
+        .push_back(//x4x
+            PortType(//x4x
+                "livelinessStates",//x4x
+                Directions::SELF//x4x
+            )//x4x
+        );//x4x
 
-    std::vector< Directions > otherDirs {//xx
-        Directions::NORTH,//xx
-        Directions::EAST,//xx
-        Directions::SOUTH,//xx
-        Directions::WEST//xx
-    };//xx
-    for( Directions d : otherDirs )//xx
-    {//xx
-        golRuleStencil.inPorts//xx
-            .push_back(//xx
-                PortType(//xx
-                    "livelinessStates",//xx
-                    d//xx
-                )//xx
-            );//xx
-    }//xx
+    std::vector< Directions > otherDirs {//x4x
+        Directions::NORTH,//x4x
+        Directions::EAST,//x4x
+        Directions::SOUTH,//x4x
+        Directions::WEST//x4x
+    };//x4x
+    for( Directions d : otherDirs )//x4x
+    {//x4x
+        golRuleStencil.inPorts//x4x
+            .push_back(//x4x
+                PortType(//x4x
+                    "livelinessStates",//x4x
+                    d//x4x
+                )//x4x
+            );//x4x
+    }//x4x
 
-    dodo::model::routine::ComponentTemplate<//xx
-        GOLPrint< DataAbstraction >//xx
-    > golPrintStencil;//xx
-    golPrintStencil.name = "print";//xx
-    golPrintStencil.predecessors.insert( golRuleStencil.name );//xx
-    golPrintStencil.inPorts.push_back(//xx
-        PortType(//xx
-            "livelinessStates",//xx
-            Directions::SELF//xx
-        )//xx
-    );//xx
+    dodo::model::routine::ComponentTemplate<//x4x
+        GOLPrint< DataAbstraction >//x4x
+    > golPrintStencil;//x4x
+    golPrintStencil.name = "print";//x4x
+    golPrintStencil.predecessors.insert( golRuleStencil.name );//x4x
+    golPrintStencil.inPorts.push_back(//x4x
+        PortType(//x4x
+            "livelinessStates",//x4x
+            Directions::SELF//x4x
+        )//x4x
+    );//x4x
 
-    using RoutineModel = dodo::model::routine::Abstraction< PhysicalDomain >;//xx
-    auto routineModel = std::make_shared< RoutineModel >( dataAbstraction );//xx
-    routineModel->instantiateComponents(//xx
-        golRuleStencil,//xx
-        golPrintStencil//xx
-    );//xx
+    using RoutineModel = dodo::model::routine::Abstraction< PhysicalDomain >;//x4x
+    auto routineModel = std::make_shared< RoutineModel >( dataAbstraction );//x4x
+    routineModel->instantiateComponents(//x4x
+        golRuleStencil,//x4x
+        golPrintStencil//x4x
+    );//x4x
 
-    auto componentMap = dodo::mapping::component2worker::generateRoundRobin(//xx
-        routineModel,//xx
-        workerModel//xx
-    );//xx
+    auto componentMap = dodo::mapping::component2worker::generateRoundRobin(//x4x
+        routineModel,//x4x
+        workerModel//x4x
+    );//x4x
 
-    dodo::mapping::component2worker::Interface< PhysicalDomain > componentMapping(//xx
-        routineModel,//xx
-        workerModel,//xx
-        componentMap//xx
-    );//xx
+    dodo::mapping::component2worker::Interface< PhysicalDomain > componentMapping(//x4x
+        routineModel,//x4x
+        workerModel,//x4x
+        componentMap//x4x
+    );//x4x
 
     // Start simulation
     for( int timestep = 0 ; timestep < 10 ; ++timestep )
